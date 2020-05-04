564d10c3844270bb5866e4f68dbaf37a808789f8
Romain <romain.geffrault@laposte.net>
2020-05-04 16:35:19 +0200

    wizishop to boutique sommeil

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WizishopTest
+# Boutique sommeil
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WizishopTest'`, () => {
+  it(`should have as title 'Boutique sommeil'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WizishopTest');
+    expect(app.title).toEqual('Boutique sommeil');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('WizishopTest app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Boutique sommeil app is running!');
   });
 });
